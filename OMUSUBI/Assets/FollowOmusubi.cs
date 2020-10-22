adf8f2259e75c8a7943621e0b6c0815fc083abf5
Mitsuru_Watanabe <m011932572@edu.teu.ac.jp>
2020-10-22 22:12:08 +0900

    First Unity

---
+++ OMUSUBI/Assets/FollowOmusubi.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowOmusubi : MonoBehaviour
+{
+    public GameObject Omusubi;
+    public Transform OmuTf;
+
+    void Start()
+    {
+        OmuTf = Omusubi.GetComponent<Transform>();
+    }
+    
+    void Update()
+    {
+        transform.position = new Vector3(OmuTf.position.x - 8.0f, Mathf.Tan(Mathf.PI / 12) * OmuTf.position.x + 2.0f, -10.0f);
+    }
+}
---
+++ OMUSUBI/Assets/KilledKARI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KilledKARI : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        //*
+        if (collision.gameObject.tag == "Rock")
+        {
+            Debug.Log("a");
+
+            //transform.position = new Vector3(58.0f, 23.0f, 0.0f);
+            Destroy(gameObject);
+        }
+        //*/
+    }
+}
